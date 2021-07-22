0fcdad9fc7409849facf2232a23869347b36ed58
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-07-22 10:56:55 +0200

    CONFIG: .env variables added

--- db.js
+++ db.js
@@ -1,19 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const DB_URL = "mongodb://localhost:27017/cesar-melia-db";
+const DB_URL = process.env.DB_URL;
 
 const connect = () => {
-    mongoose
-        .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then((res) => {
-            console.log("Connected to the database");
-        })
-        .catch((error) => {
-            console.log("A problem occurred while connecting to the database", error);
-        });
+  mongoose
+    .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(res => {
+      console.log('Connected to the database');
+    })
+    .catch(error => {
+      console.log('A problem occurred while connecting to the database', error);
+    });
 };
 
 module.exports = {
-    DB_URL,
-    connect,
+  DB_URL,
+  connect,
 };
--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-const express = require("express");
-const indexRoutes = require("./routes/index.routes");
-const projectsRoutes = require("./routes/projects.routes");
-const db = require("./db.js");
+const express = require('express');
+const indexRoutes = require('./routes/index.routes');
+const projectsRoutes = require('./routes/projects.routes');
+const db = require('./db.js');
+require('dotenv').config();
 
 db.connect();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 3500;
 
 server = express();
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.use("/", indexRoutes);
-server.use("/projects", projectsRoutes);
+server.use('/', indexRoutes);
+server.use('/projects', projectsRoutes);
 
-server.use("*", (req, res) => {
-    const error = new Error("Ruta no encontrada");
-    error.status = 404;
+server.use('*', (req, res) => {
+  const error = new Error('Ruta no encontrada');
+  error.status = 404;
 
-    return res.status(404).json(error);
+  return res.status(404).json(error);
 });
 
 server.use((error, req, res, next) => {
-    return res.status(error.status || 500).json(error.message || "Unexpected error");
+  return res.status(error.status || 500).json(error.message || 'Unexpected error');
 });
 
 server.listen(PORT, () => {
-    console.log(`Server listening in port: ${PORT}`);
+  console.log(`Server listening in port: ${PORT}`);
 });
